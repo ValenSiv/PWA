9de802525af3c27ef24c83254b1f87f8612b1149
ValenSiv <73199561+ValenSiv@users.noreply.github.com>
2022-03-01 22:53:37 -0300

    Add files via upload

--- js/parcial.js
+++ js/parcial.js
@@ -4,63 +4,63 @@ const baseDeDatos = [
         nombre: 'Camiseta Boca Juniors 1970',
         descripcion: 'Camiseta retro titular temporada 1970 con escudo',
         precio: 1000,
-        imagen: './img/remera-1.jpg'
+        imagen: './css/img/remera-1.jpg'
     },
     {
         id: 2,
         nombre: 'Camiseta Boca Juniors 1981',
         descripcion: 'Camiseta retro Diego Armando Maradona temporada 1981',
         precio: 2000,
-        imagen: './img/remera-2.jpg'
+        imagen: './css/img/remera-2.jpg'
     },
     {
         id: 3,
         nombre: 'Camiseta Boca Juniors 1991',
         descripcion: 'Camiseta retro suplente Fiat temporada 1991',
         precio: 3000,
-        imagen: './img/remera-3.jpg'
+        imagen: './css/img/remera-3.jpg'
     },
     {
         id: 4,
         nombre: 'Camiseta Boca Juniors 1987',
         descripcion: 'Camiseta retro suplente Parmalat edicion 1987',
         precio: 4000,
-        imagen: './img/remera-4.jpg'
+        imagen: './css/img/remera-4.jpg'
     },
     {
         id: 5,
         nombre: 'Camiseta Boca Juniors 1975',
         descripcion: 'Camiseta retro Chapa Suñe temporada 1970',
         precio: 5000,
-        imagen: './img/remera-5.jpg'
+        imagen: './css/img/remera-5.jpg'
     },
     {
         id: 6,
         nombre: 'Camiseta Boca Juniors 1994',
         descripcion: 'Camiseta retro titular Parmalat temporada 1994',
         precio: 6000,
-        imagen: './img/remera-6.jpg '
+        imagen: './css/img/remera-6.jpg '
     },
     {
         id: 7,
         nombre: 'Camiseta Boca Juniors 1985',
         descripcion: 'Camiseta retro titular Adidas temporada 1985',
         precio: 3500,
-        imagen: './img/remera-7.jpg'
+        imagen: './css/img/remera-7.jpg'
     },
     {
         id: 8,
         nombre: 'Camiseta Boca Juniors 2000',
         descripcion: 'Camiseta retro suplente temporada 2000',
         precio: 4500,
-        imagen: './img/remera-8.jpg'
+        imagen: './css/img/remera-8.jpg'
     },
     {
         id: 9,
         nombre: 'Camiseta Boca Juniors 2004',
         descripcion: 'Camiseta retro alternativa Megatone blanca temporada 2004',
         precio: 6000,
-        imagen: './img/remera-9.jpg'
+        imagen: './css/img/remera-9.jpg'
     }
     
 ]
